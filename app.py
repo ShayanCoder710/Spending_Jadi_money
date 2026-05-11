66d2ab6df40f0f57545e65e61bde063a13907330
ShayanCoder <shayanseyyedi24@gmail.com>
2026-05-11 21:43:31 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect(url_for('jadi'))
+
+@app.route("/jadi")
+def jadi():
+    return render_template("jadi.html")
+
+@app.route("/ElonMusk")
+def elonmusk():
+    return render_template("ElonMusk.html")
+
+@app.route("/BillGates")
+def billgates():
+    return render_template("BillGates.html")
+
+@app.route("/MarkZuckerberg")
+def markzuckerberg():
+    return render_template("MarkZuckerberg.html")
+
+@app.route("/JeffBezos")
+def jeffbezos():
+    return render_template("JeffBezos.html")
+
+@app.route("/LarryPage")
+def larrypage():
+    return render_template("LarryPage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
